b726ad51f2773d411bfe8416f1b1b305e10f4b48
hassamaslam12 <110339857+hassamaslam12@users.noreply.github.com>
2022-11-06 05:22:36 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,18 +2,18 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port =  process.env.PORT || 3000
 
 app.get('/hello', (req, res) => {
-  res.send('Hello fellows!')
+  res.send('Hello fellows! \n\n other options are / & /bye ')
   console.log(`${req.ip} is saying hello`)
 })
 app.get('/bye', (req, res) => {
-    res.send('ALLAH HAFIZ')
+    res.send('ALLAH HAFIZ  \n\n other options are /hello & / ')
     console.log(`${req.ip} is saying bye`)
 })
 app.get('/', (req, res) => {
-    res.send('kuch tou bolo')
+    res.send('kuch tou bolo  \n\n other options are /hello & /bye ')
     console.log(`${req.ip} is not saying anything`)
 })
 
